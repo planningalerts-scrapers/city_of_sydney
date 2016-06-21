5ec8d0209baec03d6886dfaf1c190b721835f7b8
Chris Nilsson <christopher@otherchirps.net>
2016-06-22 01:34:36 +1000

    Plucking content from paragraphs, instead of splitting on 'p>'

--- scraper.rb
+++ scraper.rb
@@ -5,13 +5,17 @@ require 'date'
 feed = RSS::Parser.parse("http://feeds.cityofsydney.nsw.gov.au/SydneyDAs", false)
 
 feed.channel.items.each do |item|
-  info_url = item.description.split('<a href="')[1].split('">View')[0]
+  parts = /<p>(.*)<\/p><p>(.*)<\/p><p>(.*)<\/p><p>(.*)<\/p><p>(.*)<\/p>/.match(item.description)
 
+  address, council_ref, description, closing, info_url = parts.captures
+  
+  info_url = /<a href="([^"]+)"/.match(info_url)[1]
   info_url.sub! 'DAsOnExhibition/details.asp?tpk=', 'DASearch/Detail.aspx?id='
 
-  description = item.description.split('p>')[3].gsub('</','')
-
-  exhibition_closes = item.description.split(/Exhibition Closes: \<\/strong\>/i)[1].split('</p>')[0]
+  description = description.gsub(/<\/?strong>/, '')
+  council_ref = /<strong>\s*DA Number:\s*<\/strong>(.+)/i.match(council_ref)[1]
+  exhibition_closes = /<strong>\s*Exhibition Closes:\s*<\/strong>(.+)/i.match(closing)[1]
+  
   on_notice_to = Date.parse(exhibition_closes).strftime('%Y-%m-%d')
 
   record = {
@@ -19,7 +23,7 @@ feed.channel.items.each do |item|
     "description" => description,
     "date_received" => item.pubDate.strftime('%Y-%m-%d'),
     "on_notice_to" => on_notice_to,
-    "council_reference" => item.description.split('DA Number: </strong>')[1].split('<p>')[0],
+    "council_reference" => council_ref,
     "info_url" => info_url,
     "comment_url" => "mailto:dasubmissions@cityofsydney.nsw.gov.au",
     "date_scraped" => Date.today.to_s
